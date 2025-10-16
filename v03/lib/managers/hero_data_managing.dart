6fac4463bf7b9fb41c7f6ed43dfd1a9240241f0d
Madde <105611442+SpiritualMaddie@users.noreply.github.com>
2025-10-16 20:37:08 +0200

    Revert "MVP v03 done"

---
+++ v03/lib/managers/hero_data_managing.dart
@@ -0,0 +1,11 @@
+import 'package:v03/data/models/hero_model.dart';
+
+abstract class HeroDataManaging {
+
+  Future<HeroModel> createHero(HeroModel hero);
+  Future<List<HeroModel>> getAllHeroes();
+  Future<List<HeroModel>> getHeroByName(String heroName);
+  //Future<HeroModel?> getHeroById(int id);
+  //Future<HeroModel> updateHero(HeroModel updatedHero);
+  //Future<void> deleteHero(int id);
+}
